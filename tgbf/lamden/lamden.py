f49d2e1be47affda01718f9694b4f45b7f5e28b2
endogen <081081@gmx.net>
2021-02-01 02:45:02 +0100

    Added more REST APIs

--- tgbf/lamden/lamden.py
+++ tgbf/lamden/lamden.py
@@ -1,7 +1,7 @@
 import requests
 
 from tgbf.lamden.wallet import LamdenWallet
-from contracting.client import ContractingClient
+#from contracting.client import ContractingClient
 
 
 class Lamden:
@@ -12,32 +12,64 @@ class Lamden:
         self.wallet = wallet
         self._node_url = None
 
-        # Make sure mongodb instance is running
+        # Make sure mongodb instance is running or otherwise you get this error:
+        # "Process finished with exit code 139 (interrupted by signal 11: SIGSEGV)"
         # https://blog.lamden.io/smart-contracting-with-python-2af233620dca
-        self.client = ContractingClient()
+        #self.client = ContractingClient()
 
     @property
     def node_url(self):
         self._node_url = self.host if self.port is None else f"{self.host}:{self.port}"
         return self._node_url
 
-    def get_nonce(self, address):
+    def get_nonce(self, address, raw=False):
         res = requests.get(f"{self.node_url}/nonce/{address}")
-        return res.json()["nonce"]
+        return res.json() if raw else res.json()["nonce"]
 
     def get_latest_block(self):
         res = requests.get(f"{self.node_url}/latest_block")
         return res.json()
 
-    def get_latest_block_number(self):
+    def get_latest_block_number(self, raw=False):
         res = requests.get(f"{self.node_url}/latest_block_num")
-        return res.json()["latest_block_number"]
+        return res.json() if raw else res.json()["latest_block_number"]
 
-    def get_balance(self, address):
+    def get_latest_block_hash(self):
+        res = requests.get(f"{self.node_url}/contracts/currency/methods")
+        return res.json()
+
+    def get_block_details(self, block_number):
+        res = requests.get(f"{self.node_url}/blocks?num={block_number}")
+        return res.json()
+
+    def get_balance(self, address, raw=False):
         res = requests.get(f"{self.node_url}/contracts/currency/balances?key={address}")
-        balance = res.json()["value"]
-        return "0" if balance is None else balance["__fixed__"]
+        if raw:
+            return res.json()
+        else:
+            balance = res.json()["value"]
+            return "0" if balance is None else balance["__fixed__"]  # FIXME: No '__fixed__' if on testnet
 
     def get_contracts(self):
         res = requests.get(f"{self.node_url}/contracts")
+        return res.json()
+
+    def get_transaction_details(self, hash):
+        res = requests.get(f"{self.node_url}/tx?hash={hash}")
+        return res.json()
+
+    def post_transaction(self):
+        res = requests.post(f"{self.node_url}/")
+        # TODO
+
+    def get_network_constitution(self):
+        res = requests.get(f"{self.node_url}/constitution")
+        return res.json()
+
+    def get_contract_methods(self):
+        res = requests.get(f"{self.node_url}/contracts/currency/methods")
+        return res.json()
+
+    def get_contract_variables(self):
+        res = requests.get(f"{self.node_url}/contracts/currency/methods")
         return res.json()

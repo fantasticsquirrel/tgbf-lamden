8012e69b1b93e247677a9d214bbb6ef1c7778fb6
Endogen <081081@gmx.net>
2021-02-08 09:37:16 +0100

    Reworked 'address' command to show new qr-code

--- tgbf/plugins/address/address.py
+++ tgbf/plugins/address/address.py
@@ -1,13 +1,12 @@
-import os
+import io
+import segno
 import tgbf.emoji as emo
 import tgbf.utils as utl
-import tgbf.constants as con
 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CommandHandler, CallbackContext, CallbackQueryHandler
 from telegram import ParseMode
 from tgbf.plugin import TGBFPlugin
-from MyQR import myqr
 
 
 class Address(TGBFPlugin):
@@ -29,40 +28,20 @@ class Address(TGBFPlugin):
     def address_callback(self, update: Update, context: CallbackContext):
         wallet = self.get_wallet(update.effective_user.id)
 
-        # Create directory for qr-code images
-        qr_dir = os.path.join(self.get_plg_path(), self.QRCODES_DIR)
-        os.makedirs(qr_dir, exist_ok=True)
+        buff = io.BytesIO()
+        segno.make_qr(wallet.verifying_key).save(buff, kind="png", border=1, scale=10)
 
-        # Get file and path of qr-code image
-        qr_name = f"{update.effective_user.id}.png"
-        qr_code = os.path.join(qr_dir, qr_name)
-
-        if not os.path.isfile(qr_code):
-            logo = os.path.join(self.get_plg_path(), con.DIR_RES, self.LAMDEN_LOGO)
-
-            myqr.run(
-                wallet.verifying_key,
-                version=1,
-                level='H',
-                picture=logo,
-                colorized=True,
-                contrast=1.0,
-                brightness=1.0,
-                save_name=qr_name,
-                save_dir=qr_dir)
-
-        with open(qr_code, "rb") as qr_pic:
-            if self.is_private(update.message):
-                update.message.reply_photo(
-                    photo=qr_pic,
-                    caption=f"`{wallet.verifying_key}`",
-                    parse_mode=ParseMode.MARKDOWN_V2,
-                    reply_markup=self.privkey_button_callback(wallet.signing_key))
-            else:
-                update.message.reply_photo(
-                    photo=qr_pic,
-                    caption=f"`{wallet.verifying_key}`",
-                    parse_mode=ParseMode.MARKDOWN_V2)
+        if self.is_private(update.message):
+            update.message.reply_photo(
+                photo=buff.getvalue(),
+                caption=f"`{wallet.verifying_key}`",
+                parse_mode=ParseMode.MARKDOWN_V2,
+                reply_markup=self.privkey_button_callback(wallet.signing_key))
+        else:
+            update.message.reply_photo(
+                photo=buff.getvalue(),
+                caption=f"`{wallet.verifying_key}`",
+                parse_mode=ParseMode.MARKDOWN_V2)
 
     def privkey_callback(self, update: Update, context: CallbackContext):
         query = update.callback_query

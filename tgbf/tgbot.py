ba80fd8c399f382d9fb4b1bf1928c17c5386601f
Endogen <081081@gmx.net>
2021-03-07 02:00:35 +0100

    Changed how i handle Telegram errors

--- tgbf/tgbot.py
+++ tgbf/tgbot.py
@@ -1,7 +1,10 @@
 import os
+import html
+import json
 import shutil
 import logging
 import importlib
+import traceback
 
 import tgbf.emoji as emo
 import tgbf.utils as utl
@@ -270,9 +273,31 @@ class TelegramBot:
             update.message.reply_text(msg)
 
     def _handle_tg_errors(self, update: Update, context: CallbackContext):
-        """ Handle errors for module 'telegram' and 'telegram.ext' """
-        cls_name = f"Class: {type(self).__name__}"
-        logging.error(f"{context.error} - {cls_name} - {update}")
+        """ Handle errors for modules 'telegram' and 'telegram.ext'.
+        Log the error and send a telegram message to notify the developer. """
+
+        # Log the error before we do anything else, so we can see it even if something breaks.
+        logging.error(msg="Exception while handling an update:", exc_info=context.error)
+
+        # traceback.format_exception returns the usual python message about an exception, but as a
+        # list of strings rather than a single string, so we have to join them together.
+        tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+        tb_string = ''.join(tb_list)
+
+        # Build the message with some markup and additional information about what happened.
+        # You might need to add some logic to deal with messages longer than the 4096 character limit.
+        message = (
+            f'An exception was raised while handling an update\n'
+            f'<pre>update = {html.escape(json.dumps(update.to_dict(), indent=2, ensure_ascii=False))}'
+            '</pre>\n\n'
+            f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
+            f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
+            f'<pre>{html.escape(tb_string)}</pre>'
+        )
+
+        for admin in self.config.get("admin", "ids"):
+            # Finally, send the message
+            context.bot.send_message(chat_id=admin, text=message, parse_mode=ParseMode.HTML)
 
         if not update:
             return

8d1de3e5d13cd8807dd2a5e39be4b8a9980d7805
Endogen <081081@gmx.net>
2021-03-22 02:06:42 +0100

    Added possibility to retrieve price per exchange

--- tgbf/plugins/price/price.py
+++ tgbf/plugins/price/price.py
@@ -21,6 +21,8 @@ class Price(TGBFPlugin):
     @TGBFPlugin.blacklist
     @TGBFPlugin.send_typing
     def price_callback(self, update: Update, context: CallbackContext):
+        exchange = None
+
         if len(context.args) == 1:
             if context.args[0].lower() == "jeff":
                 update.message.reply_text(
@@ -32,6 +34,8 @@ class Price(TGBFPlugin):
                     f"`1 DOUG = 1 DOUG`",
                     parse_mode=ParseMode.MARKDOWN_V2)
                 return
+            else:
+                exchange = context.args[0].lower()
 
         try:
             data = CoinGeckoAPI().get_coin_by_id(self.CGID)
@@ -49,26 +53,55 @@ class Price(TGBFPlugin):
         name = data["name"]
         symbol = data["symbol"].upper()
 
-        usd = data["market_data"]["current_price"]["usd"]
-        eur = data["market_data"]["current_price"]["eur"]
-        btc = data["market_data"]["current_price"]["btc"]
-        eth = data["market_data"]["current_price"]["eth"]
+        if exchange:
+            msg = None
 
-        p_usd = utl.format(usd, force_length=True)
-        p_eur = utl.format(eur, force_length=True, template=p_usd)
-        p_btc = utl.format(btc, force_length=True, template=p_usd)
-        p_eth = utl.format(eth, force_length=True, template=p_usd)
+            for d in data["tickers"]:
+                if exchange in d["market"]["name"].lower():
+                    usd = d["converted_last"]["usd"]
+                    btc = d["converted_last"]["btc"]
+                    eth = d["converted_last"]["eth"]
 
-        p_usd = "{:>12}".format(p_usd)
-        p_eur = "{:>12}".format(p_eur)
-        p_btc = "{:>12}".format(p_btc)
-        p_eth = "{:>12}".format(p_eth)
+                    p_usd = utl.format(usd, force_length=True)
+                    p_btc = utl.format(btc, force_length=True, template=p_usd)
+                    p_eth = utl.format(eth, force_length=True, template=p_usd)
 
-        msg = f"{name} ({symbol})\n\n" \
-              f"USD {p_usd}\n" \
-              f"EUR {p_eur}\n" \
-              f"BTC {p_btc}\n" \
-              f"ETH {p_eth}\n\n"
+                    p_usd = "{:>12}".format(p_usd)
+                    p_btc = "{:>12}".format(p_btc)
+                    p_eth = "{:>12}".format(p_eth)
+
+                    ex_name = d["market"]["name"]
+                    msg = f"{name} ({symbol}) on {ex_name}\n\n" \
+                          f"USD {p_usd}\n" \
+                          f"BTC {p_btc}\n" \
+                          f"ETH {p_eth}\n\n"
+
+            if not msg:
+                msg = f"{emo.ERROR} Unknown exchange name"
+                update.message.reply_text(msg)
+                return
+
+        else:
+            usd = data["market_data"]["current_price"]["usd"]
+            eur = data["market_data"]["current_price"]["eur"]
+            btc = data["market_data"]["current_price"]["btc"]
+            eth = data["market_data"]["current_price"]["eth"]
+
+            p_usd = utl.format(usd, force_length=True)
+            p_eur = utl.format(eur, force_length=True, template=p_usd)
+            p_btc = utl.format(btc, force_length=True, template=p_usd)
+            p_eth = utl.format(eth, force_length=True, template=p_usd)
+
+            p_usd = "{:>12}".format(p_usd)
+            p_eur = "{:>12}".format(p_eur)
+            p_btc = "{:>12}".format(p_btc)
+            p_eth = "{:>12}".format(p_eth)
+
+            msg = f"{name} ({symbol})\n\n" \
+                  f"USD {p_usd}\n" \
+                  f"EUR {p_eur}\n" \
+                  f"BTC {p_btc}\n" \
+                  f"ETH {p_eth}\n\n"
 
         update.message.reply_text(
             text=f"`{msg}`",

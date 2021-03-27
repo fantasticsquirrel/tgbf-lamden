d6d929683354ea62580c9f5ed7e7c08c9ce7f8f6
Endogen <081081@gmx.net>
2021-03-28 00:35:34 +0100

    Added more data to 'debug' command

--- tgbf/plugins/debug/debug.py
+++ tgbf/plugins/debug/debug.py
@@ -2,6 +2,7 @@ import os
 import sys
 import psutil
 import logging
+import platform
 import tgbf.utils as utl
 
 from tgbf.plugin import TGBFPlugin
@@ -9,6 +10,7 @@ from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler
 
 
+# TODO: Add endpoint URL if it is activated
 class Debug(TGBFPlugin):
 
     def load(self):
@@ -26,9 +28,27 @@ class Debug(TGBFPlugin):
         v = f"{vi.major}.{vi.minor}.{vi.micro}"
 
         msg = f"PID: {os.getpid()}\n" \
-              f"Open files: {len(open_files)}\n" \
               f"Python: {v}\n" \
-              f"IP: {utl.get_external_ip()}"
+              f"Open files: {len(open_files)}\n" \
+              f"IP: {utl.get_external_ip()}\n" \
+              f"Network: {platform.node()}\n" \
+              f"Machine: {platform.machine()}\n" \
+              f"Processor: {platform.processor()}\n" \
+              f"Platform: {platform.platform()}\n" \
+              f"OS: {platform.system()}\n" \
+              f"OS Release: {platform.release()}\n" \
+              f"OS Version: {platform.version()}\n" \
+              f"CPU Physical Cores: {psutil.cpu_count(logical=False)}\n" \
+              f"CPU Logical Cores: {psutil.cpu_count(logical=True)}\n" \
+              f"Current CPU Frequency: {psutil.cpu_freq().current}\n" \
+              f"Min CPU Frequency: {psutil.cpu_freq().min}\n" \
+              f"Max CPU Frequency: {psutil.cpu_freq().max}\n" \
+              f"CPU Utilization: {psutil.cpu_percent(interval=1)}\n" \
+              f"Per-CPU Utilization: {psutil.cpu_percent(interval=1, percpu=True)}\n" \
+              f"Total RAM: {round(psutil.virtual_memory().total/1000000000, 2)} GB\n" \
+              f"Available RAM: {round(psutil.virtual_memory().available/1000000000, 2)} GB\n" \
+              f"Used RAM: {round(psutil.virtual_memory().used/1000000000, 2)} GB\n" \
+              f"RAM Usage: {psutil.virtual_memory().percent}%"
 
         chat_info = update.effective_chat
 

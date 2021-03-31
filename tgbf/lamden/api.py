8098a28c2325a856deabe35b9c28e16738af0e0f
Endogen <081081@gmx.net>
2021-03-31 14:34:38 +0200

    Added possibility to approve other tokens than TAU and added type hints

--- tgbf/lamden/api.py
+++ tgbf/lamden/api.py
@@ -58,7 +58,7 @@ class API:
 
     # ---- Masternode API ----
 
-    def get_nonce(self, address):
+    def get_nonce(self, address: str):
         """ Get nonce to use for next transaction """
         res = requests.get(f"{self.node_url}/nonce/{address}")
         return decode(res.text)
@@ -78,12 +78,12 @@ class API:
         res = requests.get(f"{self.node_url}/latest_block_hash")
         return decode(res.text)
 
-    def get_block_details(self, block_number):
+    def get_block_details(self, block_number: Union[int, str]):
         """ Get block details for a given block number """
         res = requests.get(f"{self.node_url}/blocks?num={block_number}")
         return decode(res.text)
 
-    def get_balance(self, address=None):
+    def get_balance(self, address: str = None):
         """ Get balance for a given address or for the current
         address if no address is provided as an argument """
         if not address:
@@ -97,12 +97,12 @@ class API:
         res = requests.get(f"{self.node_url}/contracts")
         return decode(res.text)
 
-    def get_transaction_details(self, tx_hash):
+    def get_transaction_details(self, tx_hash: str):
         """ Get transaction details for given tx hash """
         res = requests.get(f"{self.node_url}/tx?hash={tx_hash}")
         return decode(res.text)
 
-    def tx_succeeded(self, tx_hash, check_period=1.5, timeout=45):
+    def tx_succeeded(self, tx_hash: str, check_period: float = 1.5, timeout: float = 45):
         end = int(time.time()) + timeout
 
         while int(time.time()) < end:
@@ -147,25 +147,25 @@ class API:
         res = requests.get(f"{self.node_url}/constitution")
         return decode(res.text)
 
-    def get_contract_methods(self, contract):
+    def get_contract_methods(self, contract: str):
         """ Get methods for a given smart contract """
         res = requests.get(f"{self.node_url}/contracts/{contract}/methods")
         return decode(res.text)
 
-    def get_contract_variables(self, contract):
+    def get_contract_variables(self, contract: str):
         """ Get variables for a given smart contract """
         res = requests.get(f"{self.node_url}/contracts/{contract}/variables")
         return decode(res.text)
 
-    def approve_contract(self, contract_name, amount: float = 100000000):
+    def approve_contract(self, contract_name: str, token: str = "currency", amount: float = 100000000):
         """ Approve smart contract to spend a specific amount of TAU """
         kwargs = {"amount": float(amount), "to": contract_name}
-        return self.post_transaction(50, "currency", "approve", kwargs)
+        return self.post_transaction(50, token, "approve", kwargs)
 
-    def get_approved_amount(self, contract_name):
+    def get_approved_amount(self, contract_name: str, token: str = "currency"):
         """ Get amount of TAU that is approved to be spent by smart contract """
         key = f"{self.wallet.verifying_key}:{contract_name}"
-        res = requests.get(f"{self.node_url}/contracts/currency/balances?key={key}")
+        res = requests.get(f"{self.node_url}/contracts/{token}/balances?key={key}")
         return decode(res.text)
 
     # ---- Block Explorer API ----

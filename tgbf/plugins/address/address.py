8b442a14210e5ee6faacf433173ce62577f75619
Endogen <081081@gmx.net>
2021-04-13 03:41:44 +0200

    Reworked 'address' command to be able to have multiple CallbackHandlers

--- tgbf/plugins/address/address.py
+++ tgbf/plugins/address/address.py
@@ -5,7 +5,7 @@ import tgbf.utils as utl
 import qrcode_artistic
 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CommandHandler, CallbackContext, CallbackQueryHandler
+from telegram.ext import CommandHandler, CallbackContext, CallbackQueryHandler, ConversationHandler
 from telegram import ParseMode
 from tgbf.plugin import TGBFPlugin
 
@@ -25,6 +25,8 @@ class Address(TGBFPlugin):
     @TGBFPlugin.blacklist
     @TGBFPlugin.send_typing
     def address_callback(self, update: Update, context: CallbackContext):
+        context.user_data.clear()
+
         wallet = self.get_wallet(update.effective_user.id)
 
         b_out = io.BytesIO()
@@ -43,11 +45,13 @@ class Address(TGBFPlugin):
             segno.make_qr(wallet.verifying_key).save(b_out, border=1, scale=10, kind="png")
 
         if self.is_private(update.message):
+            context.user_data["privkey"] = wallet.signing_key
+
             update.message.reply_photo(
                 photo=b_out.getvalue(),
                 caption=f"`{wallet.verifying_key}`",
                 parse_mode=ParseMode.MARKDOWN_V2,
-                reply_markup=self.privkey_button_callback(wallet.signing_key))
+                reply_markup=self.privkey_button_callback())
         else:
             update.message.reply_photo(
                 photo=b_out.getvalue(),
@@ -55,16 +59,19 @@ class Address(TGBFPlugin):
                 parse_mode=ParseMode.MARKDOWN_V2)
 
     def privkey_callback(self, update: Update, context: CallbackContext):
-        query = update.callback_query
-        message = query.message
+        if update.callback_query.data != self.name:
+            return
+
+        message = update.callback_query.message
+        privkey = context.user_data["privkey"]
 
         message.edit_caption(
-            caption=f"*Address*\n`{message.caption}`\n\n*Private Key*\n`{query.data}`",
+            caption=f"*Address*\n`{message.caption}`\n\n*Private Key*\n`{privkey}`",
             parse_mode=ParseMode.MARKDOWN_V2)
 
         msg = f"{emo.WARNING} DELETE AFTER VIEWING {emo.WARNING}"
-        context.bot.answer_callback_query(query.id, msg)
+        context.bot.answer_callback_query(update.callback_query.id, msg)
 
-    def privkey_button_callback(self, privkey):
-        menu = utl.build_menu([InlineKeyboardButton("Show Private Key", callback_data=privkey)])
+    def privkey_button_callback(self):
+        menu = utl.build_menu([InlineKeyboardButton("Show Private Key", callback_data=self.name)])
         return InlineKeyboardMarkup(menu, resize_keyboard=True)

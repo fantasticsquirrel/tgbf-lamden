9067d32815e3949e0f9665c3b4ff9204e8ef8243
Endogen <081081@gmx.net>
2021-05-14 17:04:35 +0200

    Show balance of all tokens in bot address

--- tgbf/plugins/balance/balance.py
+++ tgbf/plugins/balance/balance.py
@@ -1,8 +1,6 @@
-import logging
-import tgbf.utils as utl
+import tgbf.emoji as emo
 
 from telegram import Update
-from pycoingecko import CoinGeckoAPI
 from tgbf.lamden.connect import Connect
 from telegram.ext import CommandHandler, CallbackContext
 from telegram import ParseMode
@@ -23,41 +21,28 @@ class Balance(TGBFPlugin):
     @TGBFPlugin.private
     @TGBFPlugin.send_typing
     def balance_callback(self, update: Update, context: CallbackContext):
+        msg = f"{emo.HOURGLASS} Retrieving token balances..."
+        message = update.message.reply_text(msg)
+
         wallet = self.get_wallet(update.effective_user.id)
         lamden = Connect(wallet=wallet)
 
-        b = lamden.get_balance()
-        b = b["value"] if "value" in b else 0
-        b = float(str(b)) if b else float("0")
-        b = str(int(b)) if b.is_integer() else f"{b:.2f}"
+        # Find longest token symbol
+        max_length = max([len(t[0]) for t in lamden.tokens if len(t[0])])
 
-        message = update.message.reply_text(
-            text=f"`TAU: {b}`",
-            parse_mode=ParseMode.MARKDOWN_V2
+        balances = str()
+        for token in lamden.tokens:
+            b = lamden.get_balance(token[1])
+            b = b["value"] if "value" in b else 0
+            b = float(str(b)) if b else float("0")
+
+            # There is a balance for this token
+            if b > 0:
+                t_symbol = f"{token[0]}:"
+                b = str(int(b)) if b.is_integer() else f"{b:.2f}"
+                balances += f"{t_symbol:<{max_length + 1}} {b}\n"
+
+        message.edit_text(
+            text=f"<code>{balances}</code>",
+            parse_mode=ParseMode.HTML
         )
-
-        # If balance is 0, don't call CoinGecko
-        if b == "0":
-            return
-
-        try:
-            data = CoinGeckoAPI().get_coin_by_id(self.CGID)
-            prices = data["market_data"]["current_price"]
-
-            value = str()
-
-            for c in self.VS_CUR.split(","):
-                if c in prices:
-                    price = utl.format(prices[c] * float(b), decimals=2)
-                    value += f"{c.upper()}: {price}\n"
-
-            final_msg = f"`" \
-                        f"TAU: {b}\n" \
-                        f"{value}" \
-                        f"`"
-
-            message.edit_text(
-                text=final_msg,
-                parse_mode=ParseMode.MARKDOWN_V2)
-        except Exception as e:
-            logging.warning(f"Could not calculate value for user balance: {e}")

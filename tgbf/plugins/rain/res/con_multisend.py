690cfa4c9c70ee04e6adecc6db44289eeec0d813
Endogen <081081@gmx.net>
2021-05-14 17:04:01 +0200

    Remove old contract code

--- tgbf/plugins/rain/res/con_multisend.py
+++
@@ -1,6 +0,0 @@
-import currency
-
-@export
-def send(addresses: list, amount: float):
-    for address in addresses:
-        currency.transfer_from(amount=amount, to=address, main_account=ctx.signer)

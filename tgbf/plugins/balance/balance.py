f883a17594da3ae47801c7a7df51a6995d86cc7f
Endogen <081081@gmx.net>
2021-05-22 01:54:46 +0200

    Adjusted balance command to use new 'tokens' plugin

--- tgbf/plugins/balance/balance.py
+++ tgbf/plugins/balance/balance.py
@@ -10,13 +10,6 @@ from tgbf.lamden.rocketswap import Rocketswap
 class Balance(TGBFPlugin):
 
     def load(self):
-        if not self.table_exists("tokens"):
-            sql = self.get_resource("create_tokens.sql")
-            self.execute_sql(sql)
-
-        update_interval = self.config.get("update_interval")
-        self.run_repeating(self.update_tokens, update_interval)
-
         self.add_handler(CommandHandler(
             self.name,
             self.balance_callback,
@@ -36,7 +29,7 @@ class Balance(TGBFPlugin):
             if contract == "currency":
                 tau_balance.append(["TAU", b])
             else:
-                symbol = self.execute_sql(symbol_sql, contract)
+                symbol = self.execute_sql(symbol_sql, contract, plugin="tokens")
 
                 if symbol and symbol["data"]:
                     balances_list.append([symbol["data"][0][0].upper(), b])
@@ -70,24 +63,3 @@ class Balance(TGBFPlugin):
             text=f"<code>{msg}</code>",
             parse_mode=ParseMode.HTML
         )
-
-    def update_tokens(self, context: CallbackContext):
-        res = self.execute_sql(self.get_resource("select_contracts.sql"))
-
-        if res and res["data"]:
-            contracts = ["%s" % x for x in res["data"]]
-        else:
-            contracts = list()
-
-        for token in Rocketswap().token_list():
-            if token["contract_name"] not in contracts:
-                self.execute_sql(
-                    self.get_resource("insert_token.sql"),
-                    token["contract_name"],
-                    token["token_name"],
-                    token["token_symbol"],
-                    token["token_base64_png"],
-                    token["token_base64_svg"]
-                )
-
-                logging.info(f"NEW TOKEN: {token}")

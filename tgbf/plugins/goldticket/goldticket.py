7e937c91c277a5bf945321429f607e53a766f72d
endogen <ramodroll>
2021-06-01 15:59:34 +0200

    New command 'goldticket'

---
+++ tgbf/plugins/goldticket/goldticket.py
@@ -0,0 +1,115 @@
+import logging
+import tgbf.emoji as emo
+
+from telegram import Update, ParseMode
+from telegram.ext import CommandHandler, CallbackContext
+from tgbf.lamden.connect import Connect
+from tgbf.plugin import TGBFPlugin
+
+
+# TODO: No arguments needed but then how to display readme and not play directly?
+# TODO: Add 'balance' argument to check variables (tau_balance & gold_balance) of current round
+# TODO: Do we better want to trigger the winning check manually? Own argument?
+# TODO: At beginning of command check balance of TAU and GOLD and send message about won balance in case of win?
+class Goldticket(TGBFPlugin):
+
+    TOKEN_CONTRACT = "con_gold_contract"
+    TOKEN_SYMBOL = "GOLD"
+
+    def load(self):
+        self.add_handler(CommandHandler(
+            self.name,
+            self.goldticket_callback,
+            run_async=True))
+
+    @TGBFPlugin.blacklist
+    @TGBFPlugin.send_typing
+    def goldticket_callback(self, update: Update, context: CallbackContext):
+        if len(context.args) == 1 and context.args[0].lower() == "balance":
+            contract = self.config.get("contract")
+
+            b = Connect().get_balance(token=self.TOKEN_CONTRACT, contract=contract)
+            b = b["value"] if "value" in b else 0
+            b = float(str(b)) if b else float("0")
+            b = f"{int(b):,}"
+
+            update.message.reply_text(
+                text=f"<code>Balance of {contract}\n{b} {self.TOKEN_SYMBOL}</code>",
+                parse_mode=ParseMode.HTML
+            )
+
+            return
+
+        contract = self.config.get("contract")
+        function = self.config.get("function")
+
+        amount_tau = self.config.get("amount_tau")
+
+        con_msg = f"{emo.HOURGLASS} Calling contract..."
+        message = update.message.reply_text(con_msg)
+
+        usr_id = update.effective_user.id
+        wallet = self.get_wallet(usr_id)
+        lamden = Connect(wallet)
+
+        try:
+            # Check if contract is approved to spend the token
+            approved = lamden.get_approved_amount(contract, token=self.TOKEN_CONTRACT)
+            approved = approved["value"] if "value" in approved else 0
+            approved = approved if approved is not None else 0
+
+            msg = f"Approved amount of {self.TOKEN_SYMBOL} for {contract}: {approved}"
+            logging.info(msg)
+
+            if amount_tau > float(approved):
+                app = lamden.approve_contract(contract, token=self.TOKEN_CONTRACT)
+                msg = f"Approved {contract}: {app}"
+                logging.info(msg)
+        except Exception as e:
+            logging.error(f"Error approving goldticket contract: {e}")
+            message.edit_text(f"{emo.ERROR} {e}")
+            return
+
+        try:
+            # Call contract
+            flip = lamden.post_transaction(
+                stamps=50,
+                contract=contract,
+                function=function,
+                kwargs={"amount": amount_tau}
+            )
+        except Exception as e:
+            logging.error(f"Error calling goldticket contract: {e}")
+            message.edit_text(f"{emo.ERROR} {e}")
+            return
+
+        logging.info(f"Executed goldticket contract: {flip}")
+
+        if "error" in flip:
+            logging.error(f"Goldticket contract returned error: {flip['error']}")
+            message.edit_text(f"{emo.ERROR} {flip['error']}")
+            return
+
+        # Get transaction hash
+        tx_hash = flip["hash"]
+
+        # Wait for transaction to be completed
+        success, result = lamden.tx_succeeded(tx_hash)
+
+        if not success:
+            logging.error(f"Goldticket transaction not successful: {result}")
+            message.edit_text(f"{emo.ERROR} {result}")
+            return
+
+        ex_link = f'<a href="{lamden.explorer_url}/transactions/{tx_hash}">View Transaction on Explorer</a>'
+
+        if result["result"].upper() == "'YOU WON'":
+            logging.info(f"User WON <code>{amount}</code> {self.TOKEN_SYMBOL}")
+            msg = f"YOU WON <code>{amount:,}</code> {self.TOKEN_SYMBOL} {emo.MONEY_FACE}"
+        else:
+            logging.info(f"User LOST {amount} {self.TOKEN_SYMBOL}")
+            msg = f"You lost <code>{amount:,}</code> {self.TOKEN_SYMBOL} {emo.SAD} better luck next time"
+
+        message.edit_text(
+            f"{msg}\n{ex_link}",
+            parse_mode=ParseMode.HTML)

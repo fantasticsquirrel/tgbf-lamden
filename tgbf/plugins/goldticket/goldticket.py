8dfdb9bb76ca3bce56b488ff9fd8fec818211695
Endogen <081081@gmx.net>
2021-06-04 01:36:14 +0200

    Added new command 'goldticket'

--- tgbf/plugins/goldticket/goldticket.py
+++ tgbf/plugins/goldticket/goldticket.py
@@ -7,11 +7,11 @@ from telegram.ext import CommandHandler, CallbackContext, CallbackQueryHandler
 from tgbf.lamden.connect import Connect
 from tgbf.plugin import TGBFPlugin
 
-
-# TODO: No arguments needed but then how to display readme and not play directly?
-# TODO: Add 'balance' argument to check variables (tau_balance & gold_balance) of current round
-# TODO: Do we better want to trigger the winning check manually? Own argument?
-# TODO: At beginning of command check balance of TAU and GOLD and send message about won balance in case of win?
+# TODO: Make sure that only initiator can press buttons
+# TODO: 40 participants
+# TODO: Result should include amount of GOLD and TAU won and dev-fund
+# TODO: Include banner
+# TODO: Include link to https://www.tauhq.com/tokens/con_gold_contract
 class Goldticket(TGBFPlugin):
 
     TOKEN_CONTRACT = "con_gold_contract"
@@ -24,7 +24,7 @@ class Goldticket(TGBFPlugin):
             run_async=True))
 
         self.add_handler(CallbackQueryHandler(
-            self.confirm_callback,
+            self.button_callback,
             run_async=True))
 
     @TGBFPlugin.blacklist
@@ -62,12 +62,16 @@ class Goldticket(TGBFPlugin):
 
         lamden = Connect()
 
-        amount_tau = self.config.get("amount_tau")
+        gold_price = lamden.get_contract_variable(
+            "con_rocketswap_official_v1_1",
+            "prices",
+            self.TOKEN_CONTRACT
+        )
 
-        gold_price = lamden.get_contract_variable("con_rocketswap_official_v1_1", "prices")
         gold_price = gold_price["value"] if "value" in gold_price else 0
         gold_price = float(str(gold_price)) if gold_price else float("0")
 
+        amount_tau = self.config.get("amount_tau")
         amount_gold = amount_tau / gold_price
 
         context.user_data["amount_tau"] = amount_tau
@@ -75,108 +79,128 @@ class Goldticket(TGBFPlugin):
 
         message.edit_text(
             f"<code>"
-            f"Confirm deposit:\n"
+            f"Buy ticket by paying:\n"
             f"TAU:  {amount_tau}\n"
-            f"GOLD: {amount_gold}\n"
+            f"GOLD: {int(amount_gold)+1:,}\n"
             f"</code>",
             parse_mode=ParseMode.HTML,
-            reply_markup=self.button_callback("Send TAU & GOLD"))
+            reply_markup=self.get_buttons(update.effective_user.id))
 
-    def confirm_callback(self, update: Update, context: CallbackContext):
-        if update.callback_query.data != self.name:
+    def button_callback(self, update: Update, context: CallbackContext):
+        data = update.callback_query.data
+
+        if not data.startswith(self.name):
             return
 
-        amount_tau = context.user_data["amount_tau"]
-        amount_gold = context.user_data["amount_gold"]
+        data_list = data.split("|")
 
-        contract = self.config.get("contract")
-        function = self.config.get("function")
+        if int(data_list[1]) != update.effective_user.id:
+            return
 
-        message = update.callback_query.message
-        message.edit_text(f"{emo.HOURGLASS} Executing...")
+        action = data_list[2]
+        if action == "READ":
+            update.callback_query.message.edit_text(self.get_resource("goldticket.md"))
 
-        usr_id = update.effective_user.id
-        wallet = self.get_wallet(usr_id)
-        lamden = Connect(wallet)
+            msg = f"{emo.BOOKS} Happy reading!"
+            context.bot.answer_callback_query(update.callback_query.id, msg)
+            return
 
-        try:
-            # Check if contract is approved to spend GOLD
-            approved = lamden.get_approved_amount(contract, token=self.TOKEN_CONTRACT)
-            approved = approved["value"] if "value" in approved else 0
-            approved = approved if approved is not None else 0
+        elif action == "SEND":
+            amount_tau = context.user_data["amount_tau"]
+            amount_gold = context.user_data["amount_gold"]
 
-            msg = f"Approved amount of {self.TOKEN_SYMBOL} for {contract}: {approved}"
-            logging.info(msg)
+            contract = self.config.get("contract")
+            function = self.config.get("function")
 
-            if amount_tau > float(approved):
-                app = lamden.approve_contract(contract, token=self.TOKEN_CONTRACT)
-                msg = f"Approved {contract}: {app}"
+            message = update.callback_query.message
+            message.edit_text(f"{emo.HOURGLASS} Buying ticket...")
+
+            usr_id = update.effective_user.id
+            wallet = self.get_wallet(usr_id)
+            lamden = Connect(wallet)
+
+            try:
+                # Check if contract is approved to spend GOLD
+                approved = lamden.get_approved_amount(contract, token=self.TOKEN_CONTRACT)
+                approved = approved["value"] if "value" in approved else 0
+                approved = approved if approved is not None else 0
+
+                msg = f"Approved amount of {self.TOKEN_SYMBOL} for {contract}: {approved}"
                 logging.info(msg)
-        except Exception as e:
-            logging.error(f"Error approving goldticket contract: {e}")
-            message.edit_text(f"{emo.ERROR} {e}")
-            return
 
-        try:
-            # Check if contract is approved to spend TAU
-            approved = lamden.get_approved_amount(contract, token="currency")
-            approved = approved["value"] if "value" in approved else 0
-            approved = approved if approved is not None else 0
+                if amount_gold > float(approved):
+                    app = lamden.approve_contract(contract, token=self.TOKEN_CONTRACT)
+                    msg = f"Approved {contract}: {app}"
+                    logging.info(msg)
+            except Exception as e:
+                logging.error(f"Error approving goldticket contract: {e}")
+                message.edit_text(f"{emo.ERROR} {e}")
+                return
 
-            msg = f"Approved amount of TAU for {contract}: {approved}"
-            logging.info(msg)
+            try:
+                # Check if contract is approved to spend TAU
+                approved = lamden.get_approved_amount(contract, token="currency")
+                approved = approved["value"] if "value" in approved else 0
+                approved = approved if approved is not None else 0
 
-            if amount_tau > float(approved):
-                app = lamden.approve_contract(contract, token=self.TOKEN_CONTRACT)
-                msg = f"Approved {contract}: {app}"
+                msg = f"Approved amount of TAU for {contract}: {approved}"
                 logging.info(msg)
-        except Exception as e:
-            logging.error(f"Error approving goldticket contract: {e}")
-            message.edit_text(f"{emo.ERROR} {e}")
-            return
 
-        try:
-            # Call contract
-            ticket = lamden.post_transaction(
-                stamps=150,
-                contract=contract,
-                function=function,
-                kwargs={}
-            )
-        except Exception as e:
-            logging.error(f"Error calling goldticket contract: {e}")
-            message.edit_text(f"{emo.ERROR} {e}")
-            return
+                if amount_tau > float(approved):
+                    app = lamden.approve_contract(contract, token="currency")
+                    msg = f"Approved {contract}: {app}"
+                    logging.info(msg)
+            except Exception as e:
+                logging.error(f"Error approving goldticket contract: {e}")
+                message.edit_text(f"{emo.ERROR} {e}")
+                return
 
-        logging.info(f"Executed goldticket contract: {ticket}")
+            try:
+                # Call contract
+                ticket = lamden.post_transaction(
+                    stamps=100,
+                    contract=contract,
+                    function=function,
+                    kwargs={}
+                )
+            except Exception as e:
+                logging.error(f"Error calling goldticket contract: {e}")
+                message.edit_text(f"{emo.ERROR} {e}")
+                return
 
-        if "error" in ticket:
-            logging.error(f"Goldticket contract returned error: {ticket['error']}")
-            message.edit_text(f"{emo.ERROR} {ticket['error']}")
-            return
+            logging.info(f"Executed goldticket contract: {ticket}")
 
-        # Get transaction hash
-        tx_hash = ticket["hash"]
+            if "error" in ticket:
+                logging.error(f"Goldticket contract returned error: {ticket['error']}")
+                message.edit_text(f"{emo.ERROR} {ticket['error']}")
+                return
 
-        # Wait for transaction to be completed
-        success, result = lamden.tx_succeeded(tx_hash)
+            # Get transaction hash
+            tx_hash = ticket["hash"]
 
-        if not success:
-            logging.error(f"Goldticket transaction not successful: {result}")
-            message.edit_text(f"{emo.ERROR} {result}")
-            return
+            # Wait for transaction to be completed
+            success, result = lamden.tx_succeeded(tx_hash)
 
-        # TODO: Show message with comp
-        ex_link = f'<a href="{lamden.explorer_url}/transactions/{tx_hash}">View Transaction on Explorer</a>'
+            if not success:
+                logging.error(f"Goldticket transaction not successful: {result}")
+                message.edit_text(f"{emo.ERROR} {result}")
+                return
 
-        if len(result["result"]) > 2:
+            ex_link = f'<a href="{lamden.explorer_url}/transactions/{tx_hash}">View Transaction on Explorer</a>'
+
+            cur_users = len(lamden.get_contract_variable(contract, "user_list")["value"])
+            max_users = lamden.get_contract_variable(contract, "max_entries")["value"]
+
             message.edit_text(
-                f"{msg}\n{ex_link}",
+                f"Thanks entering Goldticket - you are entry {cur_users}/{max_users}\n{ex_link}",
                 parse_mode=ParseMode.HTML)
 
-        msg = f"{emo.WARNING} DELETE AFTER VIEWING {emo.WARNING}"
-        context.bot.answer_callback_query(update.callback_query.id, msg)
+            msg = f"{emo.TICKET} Ticket bought"
+            context.bot.answer_callback_query(update.callback_query.id, msg)
 
-    def button_callback(self, label: str):
-        menu = utl.build_menu([InlineKeyboardButton(label, callback_data=self.name)])
+    def get_buttons(self, user_id):
+        menu = utl.build_menu([
+            InlineKeyboardButton("Send TAU & GOLD", callback_data=f"{self.name}|{user_id}|SEND"),
+            InlineKeyboardButton("Read How-To", callback_data=f"{self.name}|{user_id}|READ")
+        ])
         return InlineKeyboardMarkup(menu, resize_keyboard=True)
--- tgbf/plugins/goldticket/res/goldticket.md
+++ tgbf/plugins/goldticket/res/goldticket.md
@@ -1,6 +1,5 @@
-This is a Lotto-style game. You pay in 10 TAU and an equivalent amount of GOLD and after 20 users participated in the game the contract randomly chooses a winner and the next round will start.  
+This is a Lotto-style game. You pay 10 TAU and an equivalent amount of GOLD and after 20 users participated in the game the contract randomly chooses a winner and the next round will start.  
 
 The winner will get 90% of all TAU and GOLD of the current round. The remaining 10% are split into 9% developer-fund that is managed by @Duckfever to support developments on Lamden and 1% goes to @Endogen for providing and maintaining the contract and bot.  
 
 10 TAU and equivalent amount of GOLD are being sent to the contract automatically. The amount of GOLD is calculated automatically based on the current price of GOLD.
-`/{{handle}}`

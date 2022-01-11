19b69b7887f5b780642494eb6de8970d1ce986cd
endogen <081081@gmx.net>
2022-01-11 17:09:24 +0100

    Made it private again to not run into "forwarding-issue"

--- tgbf/plugins/send/send.py
+++ tgbf/plugins/send/send.py
@@ -19,6 +19,7 @@ class Send(TGBFPlugin):
             self.send_callback,
             run_async=True))
 
+    @TGBFPlugin.private
     @TGBFPlugin.send_typing
     def send_callback(self, update: Update, context: CallbackContext):
         if len(context.args) != 3:

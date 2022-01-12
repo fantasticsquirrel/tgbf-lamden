21527cb6dcfb6ff0a6018590ef43416edeea4949
endogen <081081@gmx.net>
2022-01-13 00:29:31 +0100

    Added ability to refresh token data

---
+++ tgbf/plugins/tokens/res/update_token.sql
@@ -0,0 +1,3 @@
+UPDATE tokens
+SET token_name = ?, token_symbol = ?, token_base64_png = ?, token_base64_svg = ?
+WHERE contract_name = ?
--- tgbf/plugins/tokens/tokens.py
+++ tgbf/plugins/tokens/tokens.py
@@ -1,8 +1,9 @@
 import logging
 
-from telegram.ext import CallbackContext
-from tgbf.plugin import TGBFPlugin
+from telegram import Update, ParseMode
+from telegram.ext import CallbackContext, CommandHandler
 from tgbf.lamden.rocketswap import Rocketswap
+from tgbf.plugin import TGBFPlugin
 
 
 class Tokens(TGBFPlugin):
@@ -12,8 +13,29 @@ class Tokens(TGBFPlugin):
             sql = self.get_resource("create_tokens.sql")
             self.execute_sql(sql)
 
+        self.add_handler(CommandHandler(
+            self.name,
+            self.dice_callback,
+            run_async=True))
+
         update_interval = self.config.get("update_interval")
         self.run_repeating(self.update_tokens, update_interval)
+
+    @TGBFPlugin.owner
+    @TGBFPlugin.private
+    @TGBFPlugin.send_typing
+    def dice_callback(self, update: Update, context: CallbackContext):
+        if len(context.args) == 1 and context.args[0].lower() == "refresh":
+            for token in Rocketswap().token_list():
+                self.execute_sql(
+                    self.get_resource("update_token.sql"),
+                    token["token_name"],
+                    token["token_symbol"].upper(),
+                    token["token_base64_png"],
+                    token["token_base64_svg"],
+                    token["contract_name"])
+
+                logging.info(f"TOKEN REFRESHED: {token}")
 
     def update_tokens(self, context: CallbackContext):
         res = self.execute_sql(self.get_resource("select_contracts.sql"))
